f3db67109a972a387d2d52cf6b11a5d224be4577
rahuls-bidgely <55079137+rahuls-bidgely@users.noreply.github.com>
2021-03-22 19:12:08 +0530

    Create main.tf

---
+++ cassandra-na/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/home/rahul-optit/.aws/credentials"
+  profile                 = "na"
+}
+
+resource "aws_instance" "cassandra" {
+
+  ami                    = "ami-023935cef4f385f2d"
+  instance_type          = "m4.2xlarge"
+  availability_zone      = "us-east-1a"
+  key_name               = "naprod"
+  subnet_id              = "subnet-cf1687b8"
+  vpc_security_group_ids = ["sg-289a374c", "sg-554e682e", "sg-269a3742", "sg-65d35d03"]
+  iam_instance_profile   = "cloudwatchagentadminrole"
+  user_data              = "${base64encode(file("cassandraud.sh"))}"
+
+
+
+  tags = {
+    Name = "NACassCluster-1A_RackB_New_2"
+  }
+}
+resource "aws_ebs_volume" "commit-log" {
+  availability_zone = "us-east-1a"
+  size              = 40
+}
+resource "aws_volume_attachment" "commit-dir-vol" {
+  device_name = "/dev/sdb"
+  volume_id   = "${aws_ebs_volume.commit-log.id}"
+  instance_id = "${aws_instance.cassandra.id}"
+}
+resource "aws_ebs_volume" "data-dir" {
+  availability_zone = "us-east-1a"
+  size              = 1500
+}
+resource "aws_volume_attachment" "data-dir-vol" {
+  device_name = "/dev/sdc"
+  volume_id   = "${aws_ebs_volume.data-dir.id}"
+  instance_id = "${aws_instance.cassandra.id}"
+}
