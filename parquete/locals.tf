3574c7be7f4aac7e3001afcd0f082a13f0213b3a
rahuls-bidgely <55079137+rahuls-bidgely@users.noreply.github.com>
2021-06-22 12:06:39 +0530

    local file to avoid repetation

---
+++ parquete/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  image             = "ami-06499905cb26d13f2"
+  instance_profile  = "default-instance-role-prod"
+  subnet_id         = "subnet-da1687ad"
+  availability_zone = "us-east-1a"
+  key_name          = "parq"
+}
