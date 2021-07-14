87f82935db4ca3320a6a35758f67998380f58545
rahuls-bidgely <55079137+rahuls-bidgely@users.noreply.github.com>
2021-07-14 11:04:40 +0530

    create events on s3 bucket with sns as destination

---
+++ s3eventsns/s3eventsns.tf
@@ -0,0 +1,16 @@
+resource "aws_sns_topic" "topic" {
+  name = "s3-event-notification-topic"
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "var.bucket"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  topic {
+    topic_arn     = var.topic_arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = "var.filter_suffix"
+  }
+}
