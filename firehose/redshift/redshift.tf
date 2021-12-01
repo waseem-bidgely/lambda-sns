618e867db9215259d5e8c774bffe63d321aea2d5
rahuls-bidgely <55079137+rahuls-bidgely@users.noreply.github.com>
2021-12-01 11:10:55 +0530

    Update redshift.tf

--- firehose/redshift/redshift.tf
+++ firehose/redshift/redshift.tf
@@ -14,7 +14,7 @@ resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
     role_arn        = "arn:aws:iam::076900401824:role/firehose_delivery_role"
     cluster_jdbcurl = "jdbc:redshift://canada-prodca-redshiftcluster.cdozp90tzpyi.ca-central-1.redshift.amazonaws.com:5439/bdw"
     username        = "badm"
-    password        = "password"
+    password        = var.password
     data_table_name = var.name
     copy_options    = "TIMEFORMAT AS 'epochmillisecs' json 'auto'"
     s3_backup_mode  = "Disabled"
