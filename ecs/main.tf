bec6f67dcf8df434557741b5f05a0e14e15399cc
rahuls-bidgely <rahuls@bidgely.com>
2022-02-17 16:59:07 +0530

    adding env variables

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,52 +1,123 @@
 provider "aws" {
-  region                  = "us-west-2"
-  shared_credentials_file = "/home/rahul-optit/.aws/credentials"
-  profile                 = "nonprodqa"
+  region = "us-west-2"
+
 }
+
 resource "aws_ecs_task_definition" "airflow-tf" {
-  family = "airflow-tf"
-  container_definitions = jsonencode([
-    {
-      "name"             = "airflow"
-      "image"            = "189675173661.dkr.ecr.us-west-2.amazonaws.com/bidgely-airflow:0.11"
-      cpu                = 1
-      "memory"           = 2048
-      "entrypoint"       = ["bash", "-c"]
-      "command"          = ["/bin/bash -c '/opt/run-master.sh'"]
-      "workingDirectory" = "/opt/airflow"
-      "essential"        = true
-      "environment"      = var.my_env_variables
-      "portMappings" = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
+  family                = "airflow-tf"
+  container_definitions = <<DEFINITION
+ [
+   {
+     "name": "airflow",
+     "image": "189675173661.dkr.ecr.us-west-2.amazonaws.com/bidgely-airflow:0.11",
+     "cpu": 1,
+     "memory": 2048,
+     "entrypoint": ["bash", "-c"],
+     "command": ["/bin/bash -c '/opt/run-master.sh'"],
+     "workingDirectory": "/opt/airflow",
+     "essential": true,
+     "environment": [
+       {
+         "name": "AIRFLOW__CELERY__BROKER_URL",
+         "value": "${var.AIRFLOW__CELERY__BROKER_URL}"
+       },
+       {
+         "name": "AIRFLOW__CELERY__FLOWER_HOST",
+         "value": "${var.AIRFLOW__CELERY__FLOWER_HOST}"
+       },
+       {
+         "name": "AIRFLOW__CELERY__RESULT_BACKEND",
+         "value": "${var.AIRFLOW__CELERY__RESULT_BACKEND}"
+       },
+       {
+         "name": "AIRFLOW__CORE__DAG_RUN_CONF_OVERRIDES_PARAMS",
+         "value": "${var.AIRFLOW__CORE__DAG_RUN_CONF_OVERRIDES_PARAMS}"
+       },
+       {
+         "name": "AIRFLOW__CORE__EXECUTOR",
+         "value": "${var.AIRFLOW__CORE__EXECUTOR}"
+       },
+       {
+         "name": "AIRFLOW__CORE__FERNET_KEY",
+         "value": "${var.AIRFLOW__CORE__FERNET_KEY}"
+       },
+       {
+         "name": "AIRFLOW__CORE__LOAD_EXAMPLES",
+         "value": "${var.AIRFLOW__CORE__LOAD_EXAMPLES}"
+       },
+       {
+         "name": "AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER",
+         "value": "${var.AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER}"
+       },
+       {
+         "name": "AIRFLOW__CORE__REMOTE_LOGGING",
+         "value": "${var.AIRFLOW__CORE__REMOTE_LOGGING}"
+       },
+       {
+         "name": "AIRFLOW__CORE__SQL_ALCHEMY_CONN",
+         "value": "${var.AIRFLOW__CORE__SQL_ALCHEMY_CONN}"
+       },
+       {
+         "name": "AIRFLOW__OPERATORS__DEFAULT_OWNER",
+         "value": "${var.AIRFLOW__OPERATORS__DEFAULT_OWNER}"
+       },
+       {
+         "name": "AIRFLOW__WEBSERVER__AUTH_BACKEND",
+         "value": "${var.AIRFLOW__WEBSERVER__AUTH_BACKEND}"
+       },
+       {
+         "name": "AIRFLOW__WEBSERVER__AUTHENTICATE",
+         "value": "${var.AIRFLOW__WEBSERVER__AUTHENTICATE}"
+       },
+       {
+         "name": "AIRFLOW__WEBSERVER__RBAC",
+         "value": "${var.AIRFLOW__WEBSERVER__RBAC}"
+       },
+       {
+         "name": "AWS_DEFAULT_REGION",
+       "value": "${var.AWS_DEFAULT_REGION}"
+       },
+       {
+         "name": "BIDGELY_ENV",
+         "value": "${var.BIDGELY_ENV}"
+       }
+       
+     ],
+     "portMappings": [
+       {
+         "containerPort": 8080,
+         "hostPort": 8080
+       },
+       {
+         "containerPort": 5555,
+          "hostPort": 5555
+       }
+     ]
         },
         {
-          containerPort : 5555
-          hostPort : 5555
+          "name": "rabbitmq",
+          "image": "189675173661.dkr.ecr.us-west-2.amazonaws.com/bidgely-airflow:rabbitmq",
+          "entrypoint": ["bash", "-c"],
+          "command": ["bash -c '/run/run.sh'"],
+          "memory": 1024,
+          "cpu": 1,
+          "essential": true,
+          "portMappings": [
+            {
+              "containerPort": 5672,
+              "hostPort": 5672
+            },
+            {
+              "containerPort": 15672,
+              "hostPort": 15672
+            }
+          ]
+          
         }
-      ]
-    },
-    {
-      "name"       = "rabbitmq"
-      "image"      = "189675173661.dkr.ecr.us-west-2.amazonaws.com/bidgely-airflow:rabbitmq"
-      "entrypoint" = ["bash", "-c"]
-      "command"    = ["bash -c '/run/run.sh'"]
-      "memory"     = 1024
-      "essential"  = true
-      "cpu"        = 1
-      "portMappings" = [
-        {
-          containerPort : 5672
-          hostPort : 5672
-        },
-        {
-          containerPort : 15672
-          hostPort : 15672
-        }
-      ]
-    }
-  ])
+ ]
+
+DEFINITION
+
   requires_compatibilities = ["EC2"]
   network_mode             = "awsvpc"
   cpu                      = "1024"
@@ -54,3 +125,6 @@ resource "aws_ecs_task_definition" "airflow-tf" {
   task_role_arn            = var.ecs_role
   execution_role_arn       = var.ecs_role
 }
+
+
+
--- ecs/variables.tf
+++ ecs/variables.tf
@@ -1,71 +1,57 @@
 variable "ecs_role" {
   type = string
 }
-variable "my_env_variables" {
-  default = [
-    {
-      "name" : "AIRFLOW__CELERY__BROKER_URL",
-      "value" : "test"
-    },
-    {
-      "name" : "AIRFLOW__CELERY__FLOWER_HOST",
-      "value" : "airflow-master.airflow-dev-ha"
-    },
-    {
-      "name" : "AIRFLOW__CELERY__RESULT_BACKEND",
-      "value" : "db+mysql://test:test@dev-rds.cmlamxremgnb.us-west-2.rds.amazonaws.com:3306/airflowv2"
-    },
-    {
-      "name" : "AIRFLOW__CORE__DAG_RUN_CONF_OVERRIDES_PARAMS",
-      "value" : "True"
-    },
-    {
-      "name" : "AIRFLOW__CORE__EXECUTOR",
-      "value" : "CeleryExecutor"
-    },
-    {
-      "name" : "AIRFLOW__CORE__FERNET_KEY",
-      "value" : "N2OLrTTabULw8tWddt3K4A6Z-BYYPSfA7PU-lsXKG6s="
-    },
-    {
-      "name" : "AIRFLOW__CORE__LOAD_EXAMPLES",
-      "value" : "False"
-    },
-    {
-      "name" : "AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER",
-      "value" : "s3://bidgely-log-backup/airflow/logs"
-    },
-    {
-      "name" : "AIRFLOW__CORE__REMOTE_LOGGING",
-      "value" : "true"
-    },
-    {
-      "name" : "AIRFLOW__CORE__SQL_ALCHEMY_CONN",
-      "value" : "mysql://test:test@dev-rds.cmlamxremgnb.us-west-2.rds.amazonaws.com:3306/airflowv2"
-    },
-    {
-      "name" : "AIRFLOW__OPERATORS__DEFAULT_OWNER",
-      "value" : "bidgely"
-    },
-    {
-      "name" : "AIRFLOW__WEBSERVER__AUTH_BACKEND",
-      "value" : "airflow.contrib.auth.backends.password_auth"
-    },
-    {
-      "name" : "AIRFLOW__WEBSERVER__AUTHENTICATE",
-      "value" : "True"
-    },
-    {
-      "name" : "AIRFLOW__WEBSERVER__RBAC",
-      "value" : "True"
-    },
-    {
-      "name" : "AWS_DEFAULT_REGION",
-      "value" : "us-west-2"
-    },
-    {
-      "name" : "BIDGELY_ENV",
-      "value" : "dev"
-    }
-  ]
+variable "AIRFLOW__CELERY__BROKER_URL" {
+  type = string
+}
+variable "AIRFLOW__CELERY__FLOWER_HOST" {
+  type = string
+
+}
+variable "AIRFLOW__CELERY__RESULT_BACKEND" {
+  type = string
+}
+variable "AIRFLOW__CORE__DAG_RUN_CONF_OVERRIDES_PARAMS" {
+  type = string
+
+}
+variable "AIRFLOW__CORE__EXECUTOR" {
+  type = string
+}
+variable "AIRFLOW__CORE__FERNET_KEY" {
+  type = string
+
+}
+variable "AIRFLOW__CORE__LOAD_EXAMPLES" {
+  type = string
+}
+variable "AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER" {
+  type = string
+}
+variable "AIRFLOW__CORE__REMOTE_LOGGING" {
+  type = string
+
+}
+variable "AIRFLOW__CORE__SQL_ALCHEMY_CONN" {
+  type = string
+}
+variable "AIRFLOW__OPERATORS__DEFAULT_OWNER" {
+  type = string
+}
+variable "AIRFLOW__WEBSERVER__AUTH_BACKEND" {
+  type = string
+}
+variable "AIRFLOW__WEBSERVER__AUTHENTICATE" {
+  type = string
+
+}
+variable "AIRFLOW__WEBSERVER__RBAC" {
+  type = string
+}
+variable "AWS_DEFAULT_REGION" {
+  type = string
+
+}
+variable "BIDGELY_ENV" {
+  type = string
 }
